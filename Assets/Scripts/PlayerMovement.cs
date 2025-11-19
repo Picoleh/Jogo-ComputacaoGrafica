3b45d76f2f80d64830b0e4444dd22ea23d937582
Picoleh <ltpicoli@gmail.com>
2025-11-18 21:05:30 -0300

    Remodeled movement script physics

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class PlayerMovement : MonoBehaviour, ISaveable{
     [SerializeField] private CharacterController controller;
     [SerializeField] private Animator animator;
-    [SerializeField] private float moveSpeed = 6.0f;
-    [SerializeField] private float _sprintSpeed;
+    [SerializeField] private float acceleration = 12.0f;
+    [SerializeField] private float maxMoveSpeed = 6.0f;
+    [SerializeField] private float maxSprintSpeed = 12.0f;
     [SerializeField] private float rotationSpeed = 90f;
     [SerializeField] private GameObject inventoryUI;
-    private Vector3 velocity;
+    private Vector3 horizontalVelocity;
+    private float verticalVelocity;
     private float rotateInput;
 
     [SerializeField] private InputActionReference move;
@@ -21,48 +24,81 @@ public class PlayerMovement : MonoBehaviour, ISaveable{
 
     private void Start() {
         Cursor.lockState = CursorLockMode.Locked;
-        velocity = Vector3.zero;
+        horizontalVelocity = Vector3.zero;
+        verticalVelocity = 0;
         isSprinting = false;
     }
 
-    // Update is called once per frame
     void Update(){
 
-        if (Mathf.Abs(rotateInput) > 0.01f) {
-            float rotationPenalty = isSprinting ? 0.4f : 1.0f;
-            transform.Rotate(Vector3.up, rotateInput * (rotationSpeed * rotationPenalty) * Time.deltaTime);
+        if (Mathf.Abs(rotateInput) > 0.01f) { // Testa se há input de rotação
+            // Aplica penalização se estiver correndo
+            float rotationPenalty = isSprinting ? 0.4f : 1.0f; 
+            // Calcula quantidade
+            float rotationAmount = rotateInput * rotationSpeed * rotationPenalty * Time.deltaTime;
+            // Altera a velocidade com base na rotação
+            horizontalVelocity = Quaternion.Euler(0, rotationAmount, 0) * horizontalVelocity;
+            // Rotaciona o modelo
+            transform.Rotate(0, rotationAmount, 0);
         }
 
-        if (move == null || move.action == null) {
-            Debug.LogWarning("Move InputActionReference is null!");
-            return;
-        }
-        float speed = isSprinting ? _sprintSpeed : moveSpeed;
+        // Lê o input de movimento
         Vector2 _moveDirInput = move.action.ReadValue<Vector2>();
         var horizontalMove = new Vector3(_moveDirInput.x, 0, _moveDirInput.y);
-        horizontalMove = transform.TransformDirection(horizontalMove);
-        horizontalMove = horizontalMove * speed;
-        animator.SetFloat("Speed", horizontalMove.magnitude);
 
-        if(horizontalMove.magnitude > 10.0f) {
+        // Transforma velocidade global para local
+        Vector3 localVelocity = transform.InverseTransformDirection(horizontalVelocity);
+
+        float maxSpeed = isSprinting ? maxSprintSpeed : maxMoveSpeed;
+        float currentSpeed = horizontalVelocity.magnitude;
+
+        if (horizontalMove.magnitude > 0.1f) { // Se houve input
+
+            Vector3 localForward = transform.forward * _moveDirInput.y + transform.right * _moveDirInput.x;
+            float customAcceleration = acceleration;
+
+            localForward.Normalize();
+
+            if(currentSpeed < maxSpeed) {
+                customAcceleration *= Mathf.Lerp(6f, 1f, currentSpeed / maxSpeed);
+            }
+
+            // Produto vetorial para ver se nova direção é contraria a atual
+            float dot = Vector3.Dot(horizontalVelocity.normalized, localForward);
+
+            if (dot < -0.1f) // Se dot < 0 --> direção contraria
+                customAcceleration *= 6;
+
+            horizontalVelocity = Vector3.MoveTowards(horizontalVelocity, localForward * maxSpeed, customAcceleration * Time.deltaTime);
+        }
+        else { // Sem inputs
+            horizontalVelocity = Vector3.MoveTowards(horizontalVelocity, Vector3.zero, acceleration * 6 * Time.deltaTime);
+        }
+
+        animator.SetFloat("Speed", currentSpeed);
+        if(currentSpeed > maxMoveSpeed) {
             SoundManager.instance.PlayLoop("RoboCorrendo");
         }
-        else if(horizontalMove.magnitude > 5.0f) {
+        else if(currentSpeed > 1.0f) {
             SoundManager.instance.PlayLoop("RoboAndando");
         }
         else {
             SoundManager.instance.StopLoop();
         }
 
-            velocity = new Vector3(horizontalMove.x, velocity.y, horizontalMove.z);
+        Vector3 finalVelocity = new Vector3(horizontalVelocity.x, verticalVelocity, horizontalVelocity.z);
 
-        velocity.y += Physics.gravity.y * Time.deltaTime;
+        if (controller.isGrounded && verticalVelocity < 0) {
+            verticalVelocity = -2f;
+        }
 
-        controller.Move(velocity * Time.deltaTime);
+        verticalVelocity += Physics.gravity.y * Time.deltaTime;
+
+        controller.Move(finalVelocity * Time.deltaTime);
     }
 
     public void OnSprint(InputAction.CallbackContext context) {
-        if (context.performed && Mathf.Abs(velocity.x) > 0) {
+        if (context.performed && horizontalVelocity.magnitude > 0) {
             isSprinting = true;
         }
         else if (context.canceled) {
